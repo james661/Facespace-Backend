5bf7417a9a2da53788e191883676b6661b43cbee
A Friend <jamesshami13@gmail.com>
2023-07-24 13:06:43 -0700

    Added necessary files and started models

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function 
+      }
+    }
+  }
+)
